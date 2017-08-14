e14eee8b513557f4125da7a3a566d0cb449920f3
lucas <lman817@gmail.com>
2017-08-14 15:05:58 -0700

    fix

--- src/tasks.js
+++ src/tasks.js
@@ -72,7 +72,7 @@ export async function finishedScan(data) {
       console.log("inserting data: " + JSON.stringify(placement))
       await client.query('INSERT INTO placement (page, link, started, new, title, top, "left", \
                           height, width, font_size, section, screenshot) \
-                          VALUES ($1, $2, now(), TRUE, $3, $4, $5, $6, $7, $8, $9. $10) \
+                          VALUES ($1, $2, now(), TRUE, $3, $4, $5, $6, $7, $8, $9, $10) \
                           ON CONFLICT (page, link, title, top, font_size) DO UPDATE SET ended = NULL', 
                           [data.url, placement.url, placement.title, placement.top, placement.left, 
                             placement.height, placement.width, placement.fontSize, 
--- src/test.js
+++ src/test.js
@@ -3,8 +3,5 @@ console.log("Running tests")
 
 process.on('unhandledRejection', r => console.log(r));
 
+//trigger('check', {"url": "http://www.washingtonpost.com",   "linkRegex": ".*washingtonpost.com/?.*/20[0-9][0-9]/.*" })
 trigger('check', {"url": "http://www.nytimes.com",   "linkRegex": ".*nytimes.com/?.*/20[0-9][0-9]/.*" })
-.then(() => {
-  console.log("Finished scanning")
-})
-.catch(err => console.log(err))
