fd379bb70f716c30f8f82521890ca4793c3ed894
lucas <lman817@gmail.com>
2017-08-20 18:11:53 -0700

    api platform selection

--- src/api.js
+++ src/api.js
@@ -21,7 +21,7 @@ const renderPage = function (body) {
   `
 };
 
-const getArticles = async function(count, offset, name) {
+const getArticles = async function(count, offset, name, platform) {
   const articles = []  
 
   const client = new Client()
@@ -29,18 +29,22 @@ const getArticles = async function(count, offset, name) {
   try {
     console.log("loading articles")
     let vars = [count, offset]
-    let query = "SELECT MIN(placement.started) as first_seen, placement.scan_name, \
+    let query = "SELECT MIN(placement.started) as first_seen, scan.platform, placement.scan_name, \
     placement.top, placement.url, version.title, version.timestamp, version.keywords, \
-    version.generated_keywords FROM placement, version, (SELECT url, max(timestamp) as timestamp \
+    version.generated_keywords FROM placement, version, scan, (SELECT url, max(timestamp) as timestamp \
     FROM version GROUP BY url) v, \
     (SELECT url, scan_name, min(top) as top FROM placement WHERE ended IS NULL GROUP BY url, scan_name) t \
     WHERE t.top = placement.top AND t.url = placement.url AND t.scan_name = placement.scan_name \
-    AND v.url = placement.url AND version.timestamp = v.timestamp"
+    AND v.url = placement.url AND version.timestamp = v.timestamp AND scan.id = placement.scan_id"
     if (name) {
-      query += " AND placement.scan_name = $3"
+      query += " AND placement.scan_name = $" + (vars.length + 1).toString()
       vars.push(name)
     } 
-    query += " GROUP BY placement.scan_name, placement.top, placement.url, version.title, version.timestamp, \
+    if (platform) {
+      query += " AND scan.platform = $"  + (vars.length + 1).toString()
+      vars.push(platform)
+    } 
+    query += " GROUP BY placement.scan_name, scan.platform, placement.top, placement.url, version.title, version.timestamp, \
     version.keywords, version.generated_keywords ORDER BY top ASC LIMIT $1 OFFSET $2"
     const res = await client.query(query, vars)
     console.log("loaded " + res.rows.length.toString() + " articles")
@@ -65,7 +69,8 @@ api.get('/', async (request) => {
   const count = request.queryString.count || 50
   const page = request.queryString.page || 1
   const name = request.queryString.name
-  const articles = await getArticles(count, (page-1) * count, name)
+  const platform = request.queryString.platform
+  const articles = await getArticles(count, (page-1) * count, name, platform)
   return renderPage(articles.map(a => "<div><a href='" + a.url + "'>" + a.title + "</a> " + 
     " <span>" + (a.since ? moment(a.since).fromNow() : '') + "</span> " +
     "</div>").join(""))
